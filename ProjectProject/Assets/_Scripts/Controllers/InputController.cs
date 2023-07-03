b2d53cdecf7ba704cbc7634246c887333e87a507
Mightgami <vaciosimo@gmail.com>
2023-07-03 22:17:50 +0200

    Add decceleration

--- ProjectProject/Assets/_Scripts/Controllers/InputController.cs
+++ ProjectProject/Assets/_Scripts/Controllers/InputController.cs
@@ -9,22 +9,9 @@ public class InputController : Singleton<InputController>
     private void Update()
     {
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            EventManager<Vector2>.Instance.TriggerEvent("leftMovement", Vector2.left);
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
-        {
-            EventManager<Vector2>.Instance.TriggerEvent("leftMovement", Vector2.zero);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            EventManager<Vector2>.Instance.TriggerEvent("rightMovement", Vector2.right);
-        }
-        else if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
-        {
-            EventManager<Vector2>.Instance.TriggerEvent("rightMovement", Vector2.zero);
-        }
+        float direction = Input.GetAxisRaw("Horizontal");
+        EventManager<float>.Instance.TriggerEvent("movement", direction);
+
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             EventManager<bool>.Instance.TriggerEvent("jumpMovement",true);
--- ProjectProject/Assets/_Scripts/Player/Player.cs
+++ ProjectProject/Assets/_Scripts/Player/Player.cs
@@ -8,10 +8,11 @@ public class Player : MonoBehaviour
 
     public float gravity;
     public Vector2 velocity;
-    public Vector2 direction = Vector2.zero;
+    public float direction = 0;
     public float maxVelocity;
     public float speed = 2f;
     public float acceleration;
+    public float deAcceleration;
     public float maxAcceleration;
     public float jumpVelocity =20;
     public float groundHeight = 10;
@@ -24,21 +25,20 @@ public class Player : MonoBehaviour
 
     private void OnEnable()
     {
-        EventManager<Vector2>.Instance.StartListening("leftMovement",Move);
-        EventManager<Vector2>.Instance.StartListening("rightMovement", Move);
+        EventManager<float>.Instance.StartListening("movement", Move);
         EventManager<bool>.Instance.StartListening("jumpMovement", Jump);
     }
     private void OnDisable()
     {
-        EventManager<Vector2>.Instance.StopListening("leftMovement", Move);
-        EventManager<Vector2>.Instance.StopListening("rightMovement", Move);
+        EventManager<float>.Instance.StopListening("movement", Move);
         EventManager<bool>.Instance.StopListening("jumpMovement", Jump);
     }
 
-    private void Move(Vector2 movementDirection)
+    private void Move(float movementDirection)
     {
-        velocity.x = movementDirection.x * speed;
-        EventManager<float>.Instance.TriggerEvent("onPlayerChangeXVelociy", velocity.x);
+        //velocity.x = movementDirection.x * speed;
+        direction = movementDirection;
+        
     }
 
     private void Jump(bool isJumping)
@@ -50,22 +50,40 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+  
+        if (direction == 0f)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deAcceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x += acceleration * direction * Time.deltaTime;
+            velocity.x = Math.Clamp(velocity.x, -maxVelocity, maxVelocity);
+        }
+        EventManager<float>.Instance.TriggerEvent("onPlayerChangeXVelociy", velocity.x);
+
+    }
+
     private void FixedUpdate()
     {
         Vector2 pos = transform.position;
+
+
         if (!isGrounded)
         {
             pos.y += velocity.y * Time.fixedDeltaTime;
             velocity.y += gravity * Time.fixedDeltaTime;
 
-            Vector2 raycastOrigin = new Vector2((pos.x + 0.7f)* direction.x, pos.y);
+            Vector2 raycastOrigin = new Vector2((pos.x + 0.7f) * direction, pos.y);
             Vector2 rayDirection = Vector2.up;
             float rayDistance = velocity.y * Time.fixedDeltaTime;
             RaycastHit2D hit2D = Physics2D.Raycast(raycastOrigin, rayDirection, rayDistance);
-            if(hit2D.collider != null)
+            if (hit2D.collider != null)
             {
                 LandableGround landableGround = hit2D.collider.GetComponent<LandableGround>();
-                if(landableGround != null)
+                if (landableGround != null)
                 {
                     groundHeight = landableGround.groundHeight;
                     pos.y = groundHeight;
@@ -77,7 +95,7 @@ public class Player : MonoBehaviour
         }
         if (isGrounded)
         {
-            Vector2 raycastOrigin = new Vector2((pos.x - 0.7f) * direction.x, pos.y);
+            Vector2 raycastOrigin = new Vector2((pos.x - 0.7f) * direction, pos.y);
             Vector2 rayDirection = Vector2.up;
             float rayDistance = velocity.y * Time.fixedDeltaTime;
             RaycastHit2D hit2D = Physics2D.Raycast(raycastOrigin, rayDirection, rayDistance);

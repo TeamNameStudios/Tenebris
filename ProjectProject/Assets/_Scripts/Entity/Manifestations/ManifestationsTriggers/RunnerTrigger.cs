6649102603f4429cafb5c2b706f6212b1d289bf9
Rocco Maraini <roccomaraini1999@gmail.com>
2023-08-22 17:33:17 +0200

    Levels Neighbour

--- ProjectProject/Assets/_Scripts/Entity/Manifestations/ManifestationsTriggers/RunnerTrigger.cs
+++ ProjectProject/Assets/_Scripts/Entity/Manifestations/ManifestationsTriggers/RunnerTrigger.cs
@@ -38,7 +38,7 @@ public class RunnerTrigger : ManifestationTrigger
         if (GO != null)
         {
             Gizmos.DrawWireCube(transform.position, boxSize);
-            Gizmos.DrawLine(transform.position, new Vector3(spawnPosX, transform.position.y, 0));
+            Gizmos.DrawLine(transform.position, new Vector3(spawnPosX + transform.position.x, transform.position.y, 0));
         }   
     }
 }
